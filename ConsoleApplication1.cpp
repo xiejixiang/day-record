44a68e4d355cf6dd331e6a5b03ccfba9e683806d
小浩 <148411001+xiejixiang@users.noreply.github.com>
2023-10-19 16:55:56 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿#include <cmath>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+int main()
+{
+	double a, b;
+	char ch;
+	printf("Please input coordinates of a point (x,y):\n");
+	cin >> ch >> a >> ch >> b >> ch;
+	if (a == 0 and b == 0)
+	{
+		printf("This point is the origin.");
+	}
+	if (a != 0 and b == 0)
+	{
+		printf("This point is on the x-axis.");
+	}
+	if (a == 0 and b != 0)
+	{
+		printf("This point is on the y-axis.");
+	}
+	if (a > 0 and b > 0)
+	{
+		printf("This point is in QI.");
+	}
+	if (a > 0 and b < 0)
+	{
+		printf("This point is in QIV.");
+	}
+	if (a < 0 and b>0)
+	{
+		printf("This point is in QII.");
+	}
+	if (a < 0 and b < 0)
+	{
+		printf("This point is in QIII.");
+	}
+
+
+	return 0;
+
+}
